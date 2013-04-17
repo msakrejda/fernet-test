f9904158dec42fd49dd9788fd8101acf852e8589
Maciek Sakrejda <maciek@heroku.com>
2013-04-17 13:21:07 -0700

    More consistent formatting, pull out message to variable

--- fernet-test.sh
+++ fernet-test.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-FERNET_SECRET="beuVqUxoaR9fNqJ9Fq3yCsUOFTW3l1PmJcDRZJEssco="
+fernet_secret="beuVqUxoaR9fNqJ9Fq3yCsUOFTW3l1PmJcDRZJEssco="
+message="hello world"
 
-ruby_encrypted=$(cd rb-fernet && bundle exec ./fernet.rb encrypt "$FERNET_SECRET" 'hello world')
-golang_encrypted=$(cd go-fernet && go get && ./go-fernet encrypt "$FERNET_SECRET" 'hello world')
+ruby_encrypted=$(cd rb-fernet && bundle exec ./fernet.rb encrypt "$fernet_secret" "$message")
+golang_encrypted=$(cd go-fernet && go get && ./go-fernet encrypt "$fernet_secret" "$message")
 
 echo "Ruby -> Ruby:"
-(cd rb-fernet && bundle exec ./fernet.rb decrypt "$FERNET_SECRET" "$ruby_encrypted")
+(cd rb-fernet && bundle exec ./fernet.rb decrypt "$fernet_secret" "$ruby_encrypted")
 echo "Go -> Ruby:"
-(cd rb-fernet && bundle exec ./fernet.rb decrypt "$FERNET_SECRET" "$golang_encrypted")
+(cd rb-fernet && bundle exec ./fernet.rb decrypt "$fernet_secret" "$golang_encrypted")
 echo "Go -> Go:"
-(cd go-fernet && go get && ./go-fernet decrypt "$FERNET_SECRET" "$golang_encrypted")
+(cd go-fernet && go get && ./go-fernet decrypt "$fernet_secret" "$golang_encrypted")
 echo "Ruby -> Go:"
-(cd go-fernet && go get && ./go-fernet decrypt "$FERNET_SECRET" "$ruby_encrypted")
+(cd go-fernet && go get && ./go-fernet decrypt "$fernet_secret" "$ruby_encrypted")
